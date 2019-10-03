b9d6e5666ddda4c02a1722da6d9b3f2321f18475
Nicolas Almeida <51379110+NicolasAlmeidaFranca@users.noreply.github.com>
2019-10-03 14:50:53 -0300

    Add files via upload

---
+++ listar.php
@@ -0,0 +1,24 @@
+<<?php
+$conexao = mysqli_connect("localhost","id10995407_pizzaria","pizzaria","id10995407_pizzaria");
+
+
+$query = "SELECT * FROM tb_pizza order by ds_sabor";
+
+$result = mysqli_query($conexao,$query);
+
+$registro = array(
+    'pizzas'=>array()
+);
+
+$i=0;
+
+while($linha = mysqli_fetch_assoc($result)){
+    $registro['pizzas'][$i] = array(
+        'codigo' => $linha['cd_pizza'],
+        'sabor' => $linha['ds_sabor'],
+        'valor' => $linha['vl_pizza'],
+    );
+    $i++;
+}
+
+echo json_encode($registro);
